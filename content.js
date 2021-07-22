ad82f8a59a06a041d229b2673f0398b18d933087
arajput96 <sachem2015@gmail.com>
2021-07-22 18:37:06 -0400

    Chrome Extension Complete

--- content.js
+++ content.js
@@ -27,8 +27,20 @@ replaceURL(window.location.href);
 
 if (window.location.href === "https://codesmith.io/"){
   document.body.style = "background-color: pink !important";
+  rainbowfy(document.body);
   // document.body.style = "background-image: url('cs-logo.png'); background-repeat: repeat-y";
 }
 
-
+function rainbowfy (element) {
+  if(element.hasChildNodes()) {
+    element.childNodes.forEach(rainbowfy)
+  }else if (element.nodeType === Node.TEXT_NODE) {
+    if(element.textContent.match(/codesmith/gi)){
+      const newWord = document.createElement('span');
+      newWord.innerHTML = element.textContent.replace(/(codesmith)/gi,
+        '<span class="rainbow">$1</span>')
+        element.replaceWith(newWord);
+    }
+  }
+}
 
